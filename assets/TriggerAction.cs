99acf968073c31d588a5f714ee514e3cb5cdcfa4
LitsItSchool <38472702+LitsItSchool@users.noreply.github.com>
2019-01-12 13:24:39 +0200

    Add files via upload
    1

---
+++ assets/TriggerAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAction : MonoBehaviour {
+
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.name == "Player")
+        {
+            anim.SetTrigger("startAnim");
+        }
+    }
+}
